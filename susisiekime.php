c0cac07ea882ade7fe22966b8d786b2877f017cb
dallasVln <tolkac@gmail.com>
2019-10-01 21:31:09 +0300

    Ikeliau
    Winter is coming!

--- susisiekime.php
+++ susisiekime.php
@@ -7,6 +7,7 @@
 
 		include "head-settings.php";
 	?>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
 </head>
 <body>
@@ -15,85 +16,39 @@
 
 		include "header.php";
 	?>
+	
 
-	<div class="parallax-container valign-wrapper virsus">
-		<div>
-			<h1 class = "center-align" >Das Auto</h1>
-			<p class = "center-align"> Lorem ipsum dolor sit amet.</p>
-		</div>
-		<div class="parallax"><img src="https://picsum.photos/1920/1080">
-		</div>
-	</div>
+<div class = "container">
+	<img class = "paveikslas" src="images/d2.jpg">
+	<h3 class = "antraste">Duomenys, Informacija ir Nuorodos</h3>
+	<h4 class = "antraste">Duomenys</h4>
+	<p class = "tekstas">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-	<div class="container">
-		<h1 class="orange darken-4 blue-text text-lighten-4">Antraštė</h1>
+</div>
 
+	<a href="https://novaturas.lt">Novaturas</a>
+	
 
-	<?php
-		
-
-		// echo "Pavyko!";
-		// echo "<br><br>";
-
-		$sql = "SELECT * FROM barai";
-		$result = mysqli_query($conn, $sql);
-
-		if (mysqli_num_rows($result) > 0) {
-
-			echo "<table border='1'>";
+		<form action="https://google.com/search">
+		<input type="text" name="query">
+		<button>Ieškoti</button>
+	</form>
 
 
-    // output data of each row
-			while($row = mysqli_fetch_assoc($result)) {
-				
-				// print_r($row);
-
-				// echo $row["name"] . " - " . $row["rating"];
-				// echo "<br>";
-
-				echo "<tr>";
-					echo "<td>" . $row["id"] . "</td>";
-    				echo "<td>" . $row["name"] . "</td>";
-    				echo "<td>" . $row["address"] . "</td>";
-    				echo "<td>" . $row["rating"] . "</td>";
-    			echo "</tr>";
-
-			}
-
-			echo "</table>";
-
-		} else {
-			echo "0 results";
-		}
-
-		mysqli_close($conn);
-	?>
 
 
-		<div class="row">
 
-			<div class="col s12 m12 l4 center-align">
-				<img class="responsive-img" src="https://picsum.photos/200">
-				<h3 class="center-align">Block title</h3>
-				<p class="center-align">Lorem ipsum dolor sit amet, consectetur.</p>
-			</div>
-			<div class="col s12 m6 l4 center-align">
-				<img class="responsive-img" src="https://picsum.photos/200">
-				<h3 class="center-align">Block title</h3>
-				<p class="center-align">Lorem ipsum dolor sit amet, consectetur.</p>
-			</div>
-			<div class="col s12 m6 l4 center-align">
-				<img class="responsive-img" src="https://picsum.photos/200">
-				<h3 class="center-align">Block title</h3>
-				<p class="center-align">Lorem ipsum dolor sit amet, consectetur.</p>
-			</div>
-		</div>
 
-		<img class="materialboxed responsive-img" src="https://picsum.photos/2000/1000">
 
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua.</p>
-	</div>
+
+
+
+
 
 <?php 
 
