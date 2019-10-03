1c2591631b499483b001a0d86eaf679971de5445
UtahBo <44949142+UtahBo@users.noreply.github.com>
2019-10-03 18:35:06 +0300

    updated kodas

--- apie.php
+++ apie.php
@@ -35,55 +35,197 @@
 	        <img class="responsive-img circle" src="https://picsum.photos/100">
 	        <h3 class="center-align">Ilja</h3>
 	        <p class="center-align">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
-    <ul class="collapsible">
-    <li>
-      <div class="collapsible-header">
-        <i class="material-icons">flight_takeoff</i>Susipažinkime
-        <span class="new badge" data-badge-caption="Lorem!"></span>
-      </div>
-      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</span></div>
-    </li>
-  </ul>
-	      </div>
+    
+  <div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4>Ilja</h4>
+      <p>A bunch of text</p>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Uždaryti</a>
+    </div>
+  </div>
+<button data-target="modal1" class="waves-effect waves-light btn modal-trigger">Susipažinkime</button>
+        </div>
 
 
 	      <div class="col s12 m6 l3 center-align">
 	        <img class="responsive-img circle" src="https://picsum.photos/100">
 	        <h3 class="center-align">Nikita</h3>
 	        <p class="center-align">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
-<ul class="collapsible">
-    <li>
-      <div class="collapsible-header"><i class="material-icons">flight</i>Susipažinkime</div>
-      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</span></div>
-    </li>
-    </ul>
-	      </div>
-
+  <div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4>Nikita</h4>
+      <p>A bunch of text</p>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Uždaryti</a>
+    </div>
+  </div>
+<button data-target="modal1" class="waves-effect waves-light btn modal-trigger">Susipažinkime</button>
+        </div>
 
 	      <div class="col s12 m6 l3 center-align">
 	        <img class="responsive-img circle" src="https://picsum.photos/100">
 	        <h3 class="center-align">Žymantas</h3>
 	        <p class="center-align">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
-          <ul class="collapsible">
-    <li>
-      <div class="collapsible-header"><i class="material-icons">flight</i>Susipažinkime</div>
-      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</span></div>
-    </li>
-    </ul>
-</div>
+            <div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4>Žymantas</h4>
+      <p>A bunch of text</p>
+    </div>
+    <div class="modal-footer">
+      <a href="#!" class="modal-close waves-effect waves-green btn-flat">Uždaryti</a>
+    </div>
+  </div>
+<button data-target="modal1" class="waves-effect waves-light btn modal-trigger">Susipažinkime</button>
+        </div>
+
 
           <div class="col s12 m6 l3 center-align">
           <img class="responsive-img circle" src="https://picsum.photos/100">
           <h3 class="center-align">Juta</h3>
           <p class="center-align">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
-<ul class="collapsible">
-    <li>
-      <div class="collapsible-header"><i class="material-icons">flight_land</i>Susipažinkime</div>
-      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-    </li>
-  </ul>
-	    </div>
+  <div id="modal1" class="modal">
+    <div class="modal-content">
+      <h4>Juta</h4>
+      <p>A bunch of text</p>
+    </div>
+    <div class="modal-footer">
+      <a "href="#!" class="modal-close waves-effect waves-green btn-flat">Uždaryti</a>
+        </div>
+  </div>
+<button data-target="modal1" class="waves-effect waves-light btn modal-trigger">Susipažinkime</button>
+        </div>
+</div>
 
+  <ul class="collapsible popout center-align">
+        <li>
+      <div class="collapsible-header"><i class="material-icons">business_center</i>Karjera</div>
+      <div class="collapsible-body"><span>
+<b> <span style="color:orange">DĖMESIO!</b><br>
+Ieškome dar vieno ar net dviejų šeimos narių!<br>
+Jei Tu esi savimi pasitinkintis, ambicingas, atsakingas ir turi puikius bendravimo įgūdžius, tuomet kviečiame Tave prisijungti prie mūsų veržlios komandos!<br>
+<b><span style="color:orange">DARBO POBŪDIS:</b><br>
+Bendravimas su klientais, kurie ieško kokybiško poilsio;<br>
+Nepriekaištingas kiekvieno kliento aptarnavimas;<br>
+Atsakomybė už asmeninių pardavimų augimą;<br>
+Sutarčių sudarymas.<br>
+<b><span style="color:orange">KOKIO KOLEGOS IEŠKOME?</b><br>
+Tokio, kuris vertina gerą savo bei organizacijos vardą;<br>
+Yra motyvuotas;<br>
+Turi puikius teksto rašymo įgūdžius;<br>
+Yra tikras optimistas!<br>
+Reiklus ne tik sau, bet ir kitiems;<br>
+Turi puikų humoro jausmą;<br>
+Yra beprotiškai atsakingas.<br>
+Svarbiausia ne tik tai, ką Tu moki, bet ir Tavo asmenybė!<br>
+<b><span style="color:orange">KODĖL VERTA RINKTIS MUS?</b><br>
+Nuoširdus ir draugiškas kolektyvas<br>
+Vertinama iniciatyva<br>
+Stabilus bei nuo Tavo rezultatų didėjantis atlyginimas<br>
+Nemokamos kelionės<br>
+Suteiksi KLIENTUI galimybę keliauti pigiau!<br>
+<b><span style="color:orange">RAŠYK:</b> 
+<a href="mailto:negausitutodarbo@flydom.lt?subject=Gyvenimo aprašymas vadybininko pozicijai užimti">negausitutodarbo@flydom.lt</a>
+  <br><br><br>
+
+
+  <div>
+    <h9 class="center-align"> <b>   <span style="color:orange"> ARBA UŽPILDYK ŠIĄ FORMĄ</b> </h9>
+   <form class="col s12" method="POST" action="mail.php">
+  <div class="row">
+   <div class="input-field col s4">
+          <i class="material-icons prefix">account_circle</i>
+          <input id="icon_prefix" type="text" class="validate">
+          <label for="icon_prefix">Tavo vardas</label>
+   </div>
+                
+        <div class="input-field col s4">
+          <i class="material-icons prefix">email</i>
+          <input id="icon_email" type="email" class="validate">
+         <label for="email">El pašto adresas</label>
+        </div>
+
+         <div class="input-field col s4">
+          <i class="material-icons prefix">phone</i>
+          <input id="icon_telephone" type="tel" class="validate">
+          <label for="icon_telephone">Telefono numeris</label>
+        </div>
+
+<!-- <div class="file-field input-field col s6">
+      <div class="btn">
+        <span>PRisegti gyvenimo aprašymą</span>
+        <input type="file">
+      </div>
+      <div class="file-path-wrapper">
+        <input class="file-path validate" type="text">
+      </div>
+    </div> -->
+    <div class="row">
+    <form class="col s12">
+      <div class="row center-align">
+        <div class="input-field col s6">
+          <i class="material-icons prefix">mode_edit</i>
+          <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
+          <label for="icon_prefix2">Motyvacinis laiškas</label>
+        </div>
+      </div>
+    </form>
+  </div>
+      <div>
+      <button class="btn waves-effect waves-light" type="submit" name="action" id="submit" for="submit">Siųsti
+
+<i class="material-icons right">send</i>
+</button>
+</div>
+
+<?php
+error_reporting(E_ALL);
+
+$prenom = !empty($_POST['first_name']) ? $_POST['first_name'] : NULL;
+$nom = !empty($_POST['last_name']) ? $_POST['last_name'] : NULL;
+$from = !empty($_POST['mail']) ? $_POST['mail'] : NULL;
+$msg = !empty($_POST['message']) ? $_POST['message'] : NULL;
+
+if(mail('jbutvilaitei@gmail.com', 'test', $msg))
+{
+     echo 'LTest';
+}
+else
+{
+     echo 'Testas2';
+}
+// }
+?>
+           </div>
+      </div>
+    </div>
+
+  </form>
+</div>
+
+
+</span></div>
+    </li> 
+    <li>
+      <div class="collapsible-header"><i class="material-icons">place</i>Vizija, misija ir kita</div>
+      <div class="collapsible-body"><span>
+
+<b>Apie mus</b><br>
+MB Flydom – tai į kliento poreikius orientuota specialistų komanda, turinti didelę patirtį turizmo bei aptarnavimo srityse. Įmonė buvo įkurta 2018 metais, tad drąsiai gali vadintis viena seniausių turizmo agentūrų Lietuvoje. Kartu su pagrindinėmis savo paslaugomis klientams suteikiame pridėtinę vertę, sutaupytą laiką bei ramybę.<br><br>
+
+<b>Misija</b><br>
+Padėti klientui efektyviai organizuoti jo keliones.<br><br>
+
+<b>Specialistai</b><br>
+Stengiamės dirbti tik su savo srities profesionalais. Visi darbuotojai turi rezervavimo sistemų mokymų, klientų aptarnavimo mokymų ir kitus sertifikatus, įrodančius jų profesionalumą. Dalis darbuotojų patirties sėmėsi tarptautinėse įmonėse ir dalyvaudami įvairiose užsienio bei Lietuvos projektuose. Visą savo sukauptą patirtį ir profesionalumą toliau sėkmingai vysto turizmo versle.<br><br>
+
+<b>Vizija</b><br>
+Pirmaujanti ir profesionaliausia kelionių agentūra Lietuvoje.
+      </span></div>
+    </li>
+ </ul>
 
 		<img class="materialboxed responsive-img" src="https://picsum.photos/2000/1000">
 		
