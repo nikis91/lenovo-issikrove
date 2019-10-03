71c189182d98ac3f3c207af76524bb1d648dd0f7
Nikita Deniscenko <nikitadeniscenko@Nikitas-MacBook-Pro.local>
2019-10-03 21:27:34 +0300

    new footer

--- footer.php
+++ footer.php
@@ -1,24 +1,30 @@
-<footer class="page-footer blue" style="width: 100%">
+<footer class="page-footer blue">
 		<div class="container">
 			<div class="row">
 				<div class="col l6 s12">
-					<h5 class="white-text">Footer Content</h5>
-					<p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+					<!-- <h5 class="white-text">Footer Content</h5>
+					<p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p> -->
 				</div>
 				<div class="col l4 offset-l2 s12">
-					<h5 class="white-text">Links</h5>
+					<h5 class="white-text">Let's get socialized!</h5>
 					<ul>
-						<li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-						<li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-						<li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-						<li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+						<a href="https://www.twitter.com" target="blank">
+  							<img src="destinations/twitter.jpg" style="width:42px;height:42px;border:0;">
+						</a>
+						<a href="https://www.facebook.com" target="blank">
+  							<img src="destinations/fb.jpg" style="width:42px;height:42px;border:0;">
+						</a>
+						<a href="https://www.instagram.com" target="blank">
+  							<img src="destinations/insta.jpg" style="width:42px;height:42px;border:0;">
+						</a>
+
 					</ul>
 				</div>
 			</div>
 		</div>
 		<div class="footer-copyright">
 			<div class="container">
-				© 2014 Copyright Text
+				© 2019 Coding School Crew
 				<a class="grey-text text-lighten-4 right" href="#!">More Links</a>
 			</div>
 		</div>
