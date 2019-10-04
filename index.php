271ace12064a35927c5d5490777350147a2a35c9
Zymantas-Dapkus <55973753+Zymantas-Dapkus@users.noreply.github.com>
2019-10-04 20:56:27 +0300

    full update

--- index.php
+++ index.php
@@ -36,11 +36,92 @@
     </div>
   </div>
 
-		<img class="materialboxed responsive-img" src="https://picsum.photos/2000/1000">
+    <h2>Gallery</h2>
 
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua.</p>
+<div class="container" style="width: 60%">
+
+		<div class="row card">	
+
+		<div class ="col s12 m6 l4">
+			<img src="https://picsum.photos/seed/1/1920/1080" class="responsive-img card materialboxed">
+		</div>
+			<div class ="col s12 m6 l4">
+			<img src="https://picsum.photos/seed/2/1920/1080" class="responsive-img card materialboxed">
+		</div>
+			<div class ="col s12 m6 l4">
+			<img src="https://picsum.photos/seed/3/1920/1080" class="responsive-img card materialboxed">
+		</div>
+			<div class ="col s12 m6 l4">
+			<img src="https://picsum.photos/seed/4/1920/1080" class="responsive-img card materialboxed">
+		</div>
+			<div class ="col s12 m6 l4">
+			<img src="https://picsum.photos/seed/5/1920/1080" class="responsive-img card materialboxed">
+		</div>
+			<div class ="col s12 m6 l4">
+			<img src="https://picsum.photos/seed/6/1920/1080" class="responsive-img card materialboxed">
+		</div>
 	</div>
+</div>
+	</div>
+
+
+  	<h2 class= antraste>More places to see</h2> 	
+	<div class="naujienu-sarasas">
+		<div class="naujiena">
+			<img class="materialboxed" width="500" style="border-radius: 20%;"  src="https://picsum.photos/1920/1080">
+			<h3>Antraste</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod. <a href="https://www.google.com" target="blank">Learn more</a></p>
+
+
+		</div>
+		<div class="naujiena">
+			<img class="materialboxed" width="500" style="border-radius: 20%;" src="https://picsum.photos/1920/1080">
+			<h3>Antraste</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod. <a href="https://www.google.com" target="blank">Learn more</a></p></p>
+		</div>
+		<div class="naujiena">
+			<img class="materialboxed" width="500" style="border-radius: 20%;" src="https://picsum.photos/1920/1080">
+			<h3>Antraste</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod. <a href="https://www.google.com" target="blank">Learn more</a></p></p>
+		</div>
+	</div>
+
+		<h1 style="color: white">We also offer</h1>
+
+
+<div class="offers" >
+	<div class="offer">
+		 <i class="medium material-icons">accessibility</i>	
+		 <h5>Antraste</h5>
+		 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		 tempor incididunt ut labore et dolore magna aliqua.</p>
+		 <button class="btn" style="background-color: red; font-size: 10px; margin-left: 100px; cursor: pointer;">Try</button>
+	</div>
+	<div class="offer"> 
+		 <i class="medium material-icons">airplanemode_active</i>	
+		 <h5>Antraste</h5>
+		 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		 tempor incididunt ut labore et dolore magna aliqua.</p>
+		 <button class="btn" style="background-color: red; font-size: 10px; margin-left: 100px; cursor: pointer;">Try</button>
+	</div>
+	<div class="offer">
+		 <i class="medium material-icons">call</i>	
+		 <h5>Antraste</h5>
+		 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		 tempor incididunt ut labore et dolore magna aliqua.</p>
+		 <button class="btn" style="background-color: red; font-size: 10px; margin-left: 100px; cursor: pointer;">Try</button>
+	</div>
+	<div class="offer"> 
+		 <i class="medium material-icons">thumb_up</i>	
+		 <h5>Antraste</h5>
+		 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		 tempor incididunt ut labore et dolore magna aliqua.</p>
+		 <button class="btn" style="background-color: red; font-size: 10px; margin-left: 100px; cursor: pointer;">Try</button>
+	</div>
+</div>
+
+
+
 
 <?php 
 
